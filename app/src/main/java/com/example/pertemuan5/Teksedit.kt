d2b625defc8b0d81ed3a9f90351bc79e7d381c8f
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-30 09:08:37 +0700

    add OutlinedTextField for name input

--- app/src/main/java/com/example/pertemuan5/Teksedit.kt
+++ app/src/main/java/com/example/pertemuan5/Teksedit.kt
@@ -3,6 +3,7 @@ package com.example.pertemuan5
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,7 +26,12 @@ fun FormatDataDiri(modifier: Modifier){
 
     Column (modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally){}
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
 
         )
+    }
+
+
 }
