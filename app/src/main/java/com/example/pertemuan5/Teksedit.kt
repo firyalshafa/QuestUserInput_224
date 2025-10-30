8edffd3d5147332238576b3db46dd96286291c44
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-30 09:06:57 +0700

    center align Column content horizontally

--- app/src/main/java/com/example/pertemuan5/Teksedit.kt
+++ app/src/main/java/com/example/pertemuan5/Teksedit.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import java.lang.reflect.Modifier
 import java.text.Format
@@ -24,6 +25,7 @@ fun FormatDataDiri(modifier: Modifier){
 
     Column (modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){}
 
         )
 }
