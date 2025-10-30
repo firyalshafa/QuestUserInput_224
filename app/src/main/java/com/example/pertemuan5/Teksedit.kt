bbf586113c2246448d7486f4c477096d132d84d9
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-30 08:48:43 +0700

    membuat fungsi format diri

--- app/src/main/java/com/example/pertemuan5/Teksedit.kt
+++ app/src/main/java/com/example/pertemuan5/Teksedit.kt
@@ -1,2 +1,10 @@
 package com.example.pertemuan5
 
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+import java.text.Format
+
+@Composable
+fun FormatDataDiri(modifier: Modifier){
+
+}
