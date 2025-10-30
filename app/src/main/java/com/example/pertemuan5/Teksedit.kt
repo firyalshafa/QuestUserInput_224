b7c5e7603d65b80bdf9c1418397c1d2fc47f4b62
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-30 08:51:50 +0700

    add textNama state for name input field

--- app/src/main/java/com/example/pertemuan5/Teksedit.kt
+++ app/src/main/java/com/example/pertemuan5/Teksedit.kt
@@ -1,10 +1,12 @@
 package com.example.pertemuan5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
 import java.text.Format
 
 @Composable
 fun FormatDataDiri(modifier: Modifier){
+    var textNama by remember { autoStateOf(value="") }
 
 }
