83057e3ad1bc6cd46e0978733b9362a23e4c31d8
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-30 08:53:49 +0700

    add textalamt state for name input field

--- app/src/main/java/com/example/pertemuan5/Teksedit.kt
+++ app/src/main/java/com/example/pertemuan5/Teksedit.kt
@@ -1,12 +1,14 @@
 package com.example.pertemuan5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
 import java.text.Format
 
 @Composable
 fun FormatDataDiri(modifier: Modifier){
-    var textNama by remember { autoStateOf(value="") }
+    var textNama by remember { mutableStateOf(value="") }
+    var textAlamt by remember { mutableStateOf(value = "") }
 
 }
