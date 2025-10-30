1c9be0379cbfd0b0c10e8581187ee95b7c4a4ca9
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-30 09:11:02 +0700

    apply large shape to OutlinedTextField using MaterialTheme

--- app/src/main/java/com/example/pertemuan5/Teksedit.kt
+++ app/src/main/java/com/example/pertemuan5/Teksedit.kt
@@ -3,6 +3,7 @@ package com.example.pertemuan5
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -30,6 +31,7 @@ fun FormatDataDiri(modifier: Modifier){
         OutlinedTextField(
             value = textNama,
             singleLine = true,
+            shape = MaterialTheme.shapes.large,
 
         )
     }
