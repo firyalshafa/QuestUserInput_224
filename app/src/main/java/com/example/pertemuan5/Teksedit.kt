2263015875b65a3761a0311e5680a96ba8460abe
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-01 15:32:38 +0700

    menampilkan label teks untuk setiap opsi pilihan status perkawinan

--- app/src/main/java/com/example/pertemuan5/Teksedit.kt
+++ app/src/main/java/com/example/pertemuan5/Teksedit.kt
@@ -94,6 +94,7 @@ fun FormatDataDiri(modifier: Modifier
                         onClick = { textStatus = item }
 
                     )
+                    Text(text = item)
                 }
 
             }
